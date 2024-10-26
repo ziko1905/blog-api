f231b57967f2ba6e8870d5ec6db9a780ea014d63
Karlo Cehulic <karlocehlic@gmail.com>
2024-10-27 01:34:28 +0200

    Add login endpoint Npm install passport (just install, needs config)

--- back/routes/authRouter.js
+++ back/routes/authRouter.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const router = Router();
 const authController = require("../controllers/authController");
+const passport = require("passport");
 
 router.post("/signup", authController.signupPost);
+router.post("/login", passport.authenticate());
 
 module.exports = router;

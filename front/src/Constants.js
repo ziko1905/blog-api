17f42e7de092d2ca0ad2e150622c65d18c6b4399
Karlo Cehulic <karlocehlic@gmail.com>
2024-10-29 20:56:35 +0100

    Fix url constants

--- front/src/Constants.js
+++ front/src/Constants.js
@@ -4,8 +4,8 @@ const prod = {
 
 const dev = {
   url: {
-    BASE_URL: "http://localhost:3000",
+    BASE_URL: import.meta.env.VITE_BACKEND_URL,
   },
 };
 
-export const config = process.env.NODE_ENV === "development" ? dev : prod;
+export const config = import.meta.env.MODE === "development" ? dev : prod;

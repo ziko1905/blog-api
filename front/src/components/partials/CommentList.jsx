6fcc0902ada457cc8a954c93a2e5c2ae05c1e519
Karlo Cehulic <karlocehlic@gmail.com>
2024-10-30 22:33:50 +0100

    WIP: add create new comment feat

--- front/src/components/partials/CommentList.jsx
+++ front/src/components/partials/CommentList.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { isAuth } from "../../utils/isAuth";
+
 function CommentItem({ id, userName, content }) {
   return (
     <>
@@ -10,10 +13,35 @@ function CommentItem({ id, userName, content }) {
 }
 
 function CommentList({ comments = [] }) {
+  const [windowDisp, setWindowDisp] = useState(false);
+  const [user, setUser] = useState();
+
+  async function handleNewCommentDisplay() {
+    const currUser = await isAuth();
+    if (!currUser) {
+      window.location.href = "/login";
+    }
+    setUser(currUser);
+  }
+
+  function handleNewComment() {}
+
   return (
     <>
       <div>
         <h4>Comments: {comments.length}</h4>
+        <button onClick={handleNewCommentDisplay}>New Comment</button>
+        {windowDisp && (
+          <>
+            <div className="new-comment-window">
+              <form onSubmit={handleNewComment}>
+                <textarea name="content" id="content"></textarea>
+                <button type="submit">Comment</button>
+              </form>
+              <textarea name="" id=""></textarea>
+            </div>
+          </>
+        )}
         {comments.map((comm) => {
           return <CommentItem {...comm} key={comm.id} />;
         })}
